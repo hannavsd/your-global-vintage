4976fbc05d4981b8fab0a3ad7b0cbd8c0c557dd1
Hanna Davidescu <hannadavidescu@Hannas-MacBook-Pro.local>
2025-06-08 21:38:40 +0100

    Add email field to contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,29 +5,6 @@ const Contact = () => {
   const [selectedForm, setSelectedForm] = useState("buyer");
   const [formSent, setFormSent] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-
-    try {
-      const response = await fetch("https://formsubmit.co/yourglobalvintage@gmail.com", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (response.ok) {
-        setFormSent(true);
-        e.target.reset();
-      } else {
-        alert("Failed to send form. Please try again later.");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("There was an error sending the form.");
-    }
-  };
-
   return (
     <div className="contact-container">
       <h1>Contact Us</h1>
@@ -55,23 +32,41 @@ const Contact = () => {
 
       {/* Buyer Form */}
       {selectedForm === "buyer" && !formSent && (
-        <form className="contact-form" onSubmit={handleSubmit}>
+        <form
+          className="contact-form"
+          action="https://formsubmit.co/yourglobalvintage@gmail.com"
+          method="POST"
+          encType="multipart/form-data"
+          onSubmit={() => setFormSent(true)}
+        >
           <input type="hidden" name="_captcha" value="false" />
-          <input type="hidden" name="_subject" value="New message from Your Global Vintage buyer!" />
+          <input
+            type="hidden"
+            name="_subject"
+            value="New message from Your Global Vintage buyer!"
+          />
           <input type="hidden" name="_template" value="box" />
 
           <label>
             Name:
             <input type="text" name="name" required />
           </label>
+
+          <label>
+            Email:
+            <input type="email" name="email" required />
+          </label>
+
           <label>
             Message:
             <textarea name="message" rows={4} required></textarea>
           </label>
+
           <label>
             Optional File Upload:
             <input type="file" name="attachment" />
           </label>
+
           <button type="submit">Send</button>
         </form>
       )}
@@ -80,7 +75,10 @@ const Contact = () => {
       {formSent && (
         <div className="thank-you-message">
           <h2>Thank you!</h2>
-          <p>We have received your message and will get back to you as soon as we can.</p>
+          <p>
+            We have received your message and will get back to you as soon as we
+            can.
+          </p>
         </div>
       )}
 
